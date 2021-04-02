9130cc30fca3e9eece3e608b25346646d4e6d928
ricardoelena <ricardoelena@gmail.com>
2021-04-01 21:42:21 -0400

    Create AVCheck.js
    This JS is meant to be used with the PS1 script from Appgate GitHub https://github.com/appgate/sdp-win-security-center/blob/master/avinspect.ps1

---
+++ AVCheck.js
@@ -0,0 +1,21 @@
+var AVJason;
+
+if(!claims.device.avcheck) return false;
+
+console.log(claims.device.avcheck);
+
+try{
+  	AVJason = JSON.parse(claims.device.avcheck.replace(/\\/g,"").replace(/"{/g,"{").replace(/}"/g,"}").replace(/\r\n/g,""));
+  	for (var i = 0; i < AVJason.length; i++){
+       var av = AVJason[i];
+       
+       if( av["Name"] != "No av detected." )return true;   
+     }
+  	 console.log("required state <> " + state);
+  	 return false;
+}
+catch(err){
+ 	console.log(err);
+ 	return false;
+}
+return false;
